7d955a1eb3f947070fb32898462b80780a165940
Tmoemes <Tmoemes@users.noreply.github.com>
2021-01-16 00:20:39 +0100

    comments in program.cs

--- ContainerShip/Program.cs
+++ ContainerShip/Program.cs
@@ -11,12 +11,12 @@ namespace ContainerShip
 
             var begincontainers = new List<Container>();
 
-            for (int i = 0; i < 100; i++)
+            for (int i = 0; i < 100; i++) //aantal containers dat moet worden gesorteerd
             {
                 begincontainers.Add(Container.GenerateContainer());
             }
 
-            var containership = new Ship(4,4);
+            var containership = new Ship(4,4); //groote van het schip
 
             containership.SortShip(begincontainers);
 
